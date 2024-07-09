43ecf0dfa31ec7f1a367bfd7470aac0eaa684973
MARCUS DAVID <davidmarcus827@gmail.com>
2024-07-09 19:07:40 +0100

    Marcus-branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./components/home/Navbar";
+import Hero from "./components/home/Hero";
+import MovieSection from "./components/home/MovieSection";
 
 const App = () => {
   return (
     <div>
-      
+      <Navbar />
+      <Hero />
+      <MovieSection />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
